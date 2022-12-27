a7dfe1c8afbc2885577d572fc32a9c4fbad6af64
Tolstoy Yaroslav <rastorop92@yandex.ru>
2022-12-27 17:06:02 +0300

    fix: components

--- nodes/AmoCRM/resources/leads/getList/description.ts
+++ nodes/AmoCRM/resources/leads/getList/description.ts
@@ -1,19 +1,17 @@
 import { ILeadsProperties } from '../../interfaces';
+import { addLimitDescription } from '../../_components/LimitDescription';
+import { addPageDescription } from '../../_components/PageDescription';
+import { addReturnAll } from '../../_components/ReturnAllDescription';
+import { addSortDescription } from '../../_components/SortDescription';
+import { addWithDescription } from '../../_components/WithDescription';
 
 export const description: ILeadsProperties = [
-	{
-		displayName: 'Return All',
-		name: 'returnAll',
-		type: 'boolean',
-		displayOptions: {
-			show: {
-				resource: ['leads'],
-				operation: ['getList'],
-			},
+	addReturnAll({
+		show: {
+			resource: ['leads'],
+			operation: ['getList'],
 		},
-		default: false,
-		description: 'If all results should be returned.',
-	},
+	}),
 	{
 		displayName: 'Filter',
 		name: 'filter',
@@ -274,130 +272,59 @@ export const description: ILeadsProperties = [
 			},
 		},
 		options: [
-			{
-				displayName: 'Sort',
-				name: 'sort',
-				placeholder: 'Add sorting',
-				type: 'fixedCollection',
-				default: {},
-				options: [
-					{
-						displayName: 'Sort settings',
-						name: 'sortSettings',
-						values: [
-							{
-								displayName: 'Sort By',
-								name: 'sort_by',
-								type: 'options',
-								options: [
-									{
-										name: 'Date create',
-										value: 'created_at',
-									},
-									{
-										name: 'Date update',
-										value: 'updated_at',
-									},
-									{
-										name: 'ID',
-										value: 'id',
-									},
-								],
-								default: '',
-								required: true,
-								description: 'Field to sort records by',
-							},
-							{
-								displayName: 'Sort Order',
-								name: 'sort_order',
-								type: 'options',
-								options: [
-									{
-										name: 'Ascending',
-										value: 'asc',
-									},
-									{
-										name: 'Descending',
-										value: 'desc',
-									},
-								],
-								default: '',
-								required: true,
-								description: 'Ascending or descending order sort order',
-							},
-						],
-					},
-				],
-			},
-			{
-				displayName: 'With',
-				name: 'with',
-				type: 'multiOptions',
-				default: [],
-				description: 'Select the information to be added to the response',
-				noDataExpression: true,
-				options: [
-					{
-						name: 'Deal-related catalog elements',
-						value: 'catalog_elements',
-					},
-					{
-						name: 'Last modified by robot',
-						value: 'is_price_modified_by_robot',
-					},
-					{
-						name: 'Loss reason',
-						value: 'loss_reason',
-					},
-					{
-						name: 'Contacts',
-						value: 'contacts',
-					},
-					{
-						name: 'Only deleted',
-						value: 'only_deleted',
-					},
-					{
-						name: 'Source id',
-						value: 'source_id',
-					},
-				],
-			},
+			addSortDescription([
+				{
+					name: 'Date create',
+					value: 'created_at',
+				},
+				{
+					name: 'Date update',
+					value: 'updated_at',
+				},
+				{
+					name: 'ID',
+					value: 'id',
+				},
+			]),
+			addWithDescription(undefined, [
+				{
+					name: 'Deal-related catalog elements',
+					value: 'catalog_elements',
+				},
+				{
+					name: 'Last modified by robot',
+					value: 'is_price_modified_by_robot',
+				},
+				{
+					name: 'Loss reason',
+					value: 'loss_reason',
+				},
+				{
+					name: 'Contacts',
+					value: 'contacts',
+				},
+				{
+					name: 'Only deleted',
+					value: 'only_deleted',
+				},
+				{
+					name: 'Source id',
+					value: 'source_id',
+				},
+			]),
 		],
 	},
-	{
-		displayName: 'Page',
-		name: 'page',
-		type: 'number',
-		displayOptions: {
-			show: {
-				resource: ['leads'],
-				operation: ['getList'],
-				returnAll: [false],
-			},
+	addPageDescription({
+		show: {
+			resource: ['leads'],
+			operation: ['getList'],
+			returnAll: [false],
 		},
-		typeOptions: {
-			minValue: 1,
+	}),
+	addLimitDescription({
+		show: {
+			operation: ['getList'],
+			resource: ['leads'],
 		},
-		default: 1,
-		description: 'Select page',
-	},
-	{
-		displayName: 'Limit',
-		name: 'limit',
-		type: 'number',
-		displayOptions: {
-			show: {
-				operation: ['getList'],
-				resource: ['leads'],
-			},
-		},
-		typeOptions: {
-			maxValue: 250,
-			minValue: 1,
-			numberStepSize: 1,
-		},
-		default: 50,
-		description: 'Select limit of count leads',
-	},
+	}),
 ];
