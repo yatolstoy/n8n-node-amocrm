fac3ab0c88201857a0d722cd57c5bd51f12f4196
yatolstoy <rastorop92@yandex.ru>
2023-12-20 10:26:04 +0300

    fix: delete DateDescription component

--- nodes/AmoCRM/V1/resources/_components/DateDescription.ts
+++
@@ -1,4 +0,0 @@
-export const getTimestampFromDateString = (dateString: string | undefined): number | undefined => {
-	if (!dateString) return;
-	return Math.round(new Date(dateString).valueOf() / 1000);
-};
