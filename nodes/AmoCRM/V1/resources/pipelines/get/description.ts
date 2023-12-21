630cbfb1870cd93d3d87931dc089e46e3a785e66
yatolstoy <rastorop92@yandex.ru>
2023-12-21 18:05:32 +0300

    remove trash

--- nodes/AmoCRM/V1/resources/pipelines/get/description.ts
+++
@@ -1,9 +0,0 @@
-import { IPipelinesProperties } from '../../interfaces';
-
-// const displayOptions = {
-// 	show: {
-// 		resource: ['pipelines'],
-// 		operation: ['get'],
-// 	},
-// };
-export const description: IPipelinesProperties = [];
