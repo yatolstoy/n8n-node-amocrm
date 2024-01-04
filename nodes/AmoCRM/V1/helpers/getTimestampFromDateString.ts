15578f1275bf7e0ff560d9396a45f82d9a206a82
yatolstoy <rastorop92@yandex.ru>
2024-01-04 11:02:23 +0300

    fix: custom number of dates

--- nodes/AmoCRM/V1/helpers/getTimestampFromDateString.ts
+++ nodes/AmoCRM/V1/helpers/getTimestampFromDateString.ts
@@ -1,4 +1,7 @@
-export const getTimestampFromDateString = (dateString: string | undefined): number | undefined => {
+export const getTimestampFromDateString = (
+	dateString: string | number | undefined,
+): number | undefined => {
 	if (!dateString) return;
+	if (typeof dateString === 'number') return dateString;
 	return Math.round(new Date(dateString).valueOf() / 1000);
 };
