847a2e5175d53875d937382e4ee291281ff0ea0e
Yatolstoy <rastorop92@yandex.ru>
2024-09-27 09:47:07 +0300

    refactor: cacheRequest function

--- nodes/AmoCRM/V1/helpers/cacheRequest.ts
+++ nodes/AmoCRM/V1/helpers/cacheRequest.ts
@@ -2,32 +2,37 @@ import { ILoadOptionsFunctions, INodePropertyOptions } from 'n8n-workflow';
 
 type MyFunction = (this: ILoadOptionsFunctions) => Promise<INodePropertyOptions[]>;
 
+const WAIT_TIME = 300;
+const CACHE_TIME = 5 * 1000;
+
+const cache: {
+	[key: string]: { lastUpdate: number; data: INodePropertyOptions[]; loading: boolean };
+} = {};
+
 export const cacheOptionsRequest = (fnc: MyFunction) => {
-	const WAIT_TIME = 300;
-	const CACHE_TIME = 5 * 1000;
+	const key = JSON.stringify(fnc.name);
 
-	const cache: { lastUpdate: number; data: INodePropertyOptions[]; loading: boolean } = {
-		lastUpdate: 0,
-		data: [],
-		loading: false,
-	};
-
-	const fn = async function (this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
-		if (cache.loading) {
+	return async function (this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+		if (cache[key]?.loading) {
 			await new Promise((resolve) => setTimeout(resolve, WAIT_TIME));
-			return fn.call(this);
+			return cacheOptionsRequest(fnc).call(this);
 		}
-		if (Date.now() < cache.lastUpdate + CACHE_TIME) {
-			return cache.data;
+
+		const cachedData = cache[key];
+		if (cachedData && Date.now() < cachedData.lastUpdate + CACHE_TIME) {
+			return cachedData.data;
 		}
-		cache.loading = true;
-		const data = await fnc.call(this);
 
-		cache.data = data;
-		cache.lastUpdate = Date.now();
-		cache.loading = false;
+		cache[key] = cache[key] || { lastUpdate: Date.now(), data: [], loading: true };
+		cache[key].loading = true;
 
-		return data;
+		try {
+			const data = await fnc.call(this);
+			cache[key] = { lastUpdate: Date.now(), data, loading: false };
+			return data;
+		} catch (e) {
+			cache[key].loading = false;
+			throw e;
+		}
 	};
-	return fn;
 };
